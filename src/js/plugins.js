6bb841bc6631c0a423408b306472c0ac89427f59
Daniel Sazonov <p.294803@gmail.com>
2020-12-24 20:47:12 +0300

    Refactor code

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,16 +1,16 @@
 // Avoid `console` errors in browsers that lack a console.
 (function () {
-  var method;
-  var noop = function () {
+  let method;
+  let noop = function () {
   };
-  var methods = [
+  let methods = [
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
     'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
     'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+  let length = methods.length;
+  let console = (window.console = window.console || {});
 
   while (length--) {
     method = methods[length];
