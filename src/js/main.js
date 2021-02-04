7e2b6b3d31d84c77c3f5052fc78db27407457e7c
Daniel Sazonov <p.294803@gmail.com>
2021-02-04 17:10:39 +0300

    Add test alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
-jQuery(document).ready(function($){
+$(document).ready(function(){
   console.log('Hello world');
+  $('#from-jquery').append('Если этот текст можно прочитать, значит на фронте все правильно.');
 });
