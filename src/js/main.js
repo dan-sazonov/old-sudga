e95a0933461386efd1facb8e4d84934a70e54a61
Daniel Sazonov <p.294803@gmail.com>
2021-02-11 19:27:39 +0300

    Delete unused examples

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,4 @@
-let foo = 'bar';
-const R = require('ramda');
-
-const square = function square (x) { return x * x; };
-const squares = R.chain(square, [1, 2, 3, 4, 5]);
-console.log(squares);
 $(document).ready(function(){
-  console.log(foo);
   console.log('Hello world');
   $('#from-jquery').append('Если этот текст можно прочитать, значит на фронте все правильно.');
-  $('.carousel').carousel()
 });
