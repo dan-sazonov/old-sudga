510cfef17433177ab95dfe4b0324758e243c6ad0
Daniel Sazonov <p.294803@gmail.com>
2021-05-03 17:05:48 +0300

    Create landing block for the comment widget

--- src/js/vk.js
+++ src/js/vk.js
@@ -4,6 +4,9 @@ import {debug} from './main';
 function vkInit() {
   if (debug.showVkWidget && (typeof VK !== 'undefined')) {
     VK.Widgets.Comments('vk_comments', {limit: 5, attach: '*'});
+  } else if (debug.showVkWidget) {
+    $('#vk_comments').addClass('hidden');
+    $('#vk_comments_land').removeClass('hidden');
   } else {
     $('#vk_comments').toggleClass('vk-comments-debug');
   }
