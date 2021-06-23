ad784454141798b15f23072e1717a0849e891f9e
Daniel Sazonov <p.294803@gmail.com>
2021-06-23 03:29:30 +0300

    Add different ids for vk widgets

--- src/js/vk.js
+++ src/js/vk.js
@@ -13,11 +13,12 @@ if (window.location.hostname.split('.')[0] !== 'www') {
 export function vkInit() {
   if (debug.showVkWidget && (typeof VK !== 'undefined') && allowedHosts.includes(currentHost)) {
     /* global VK */
-    VK.Widgets.Comments('vk_comments', {limit: 5, attach: '*'}, 'm0');
+    VK.Widgets.Comments('vk-comments_main', {limit: 5, attach: '*'}, 'm0');
+    VK.Widgets.Comments('vk-comments_template', {limit: 5, attach: '*'}, 'm1');
   } else if (debug.showVkWidget) {
-    $('#vk_comments').addClass('hidden');
+    $('.vk_comments').addClass('hidden');
     $('#vk_comments_land').removeClass('hidden');
   } else {
-    $('#vk_comments').toggleClass('vk-comments-debug');
+    $('.vk_comments').toggleClass('vk-comments-debug');
   }
 }
