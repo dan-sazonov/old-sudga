f4c9e699df4f02b4c3d678ab3750f1ac57f1dbd0
Daniel Sazonov <p.294803@gmail.com>
2021-06-25 03:53:52 +0300

    Set different widgets for every page

--- src/js/vk.js
+++ src/js/vk.js
@@ -3,6 +3,11 @@ import {debug} from './config';
 
 const allowedHosts = ['old-sud.ga', 'old-sudga.github.io', 'xn----7sbbgido7cwbu3ka.xn--p1acf'];
 let currentHost = '';
+const currentPage = window.location.pathname === '/' ? 'index' : window.location.pathname.split('.')[0].slice(1);
+const widgetIds = {
+  index: 'm0',
+  template: 'm1'
+};
 
 if (window.location.hostname.split('.')[0] !== 'www') {
   currentHost = window.location.hostname;
@@ -13,8 +18,7 @@ if (window.location.hostname.split('.')[0] !== 'www') {
 export function vkInit() {
   if (debug.showVkWidget && (typeof VK !== 'undefined') && allowedHosts.includes(currentHost)) {
     /* global VK */
-    VK.Widgets.Comments('vk-comments_main', {limit: 5, attach: '*'}, 'm0');
-    VK.Widgets.Comments('vk-comments_template', {limit: 5, attach: '*'}, 'm1');
+    VK.Widgets.Comments(`vk-comments_${currentPage}`, {limit: 5, attach: '*'}, widgetIds[currentPage]);
   } else if (debug.showVkWidget) {
     $('.vk_comments').addClass('hidden');
     $('#vk_comments_land').removeClass('hidden');
